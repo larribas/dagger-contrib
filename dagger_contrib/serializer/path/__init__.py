548a2f33c66f871184ea5ae21918a98066b0d105
larribas <lorenzo.s.arribas@gmail.com>
2021-09-29 09:20:24 +0200

    feat(serializer): add a path.AsZip serializer

--- dagger_contrib/serializer/path/__init__.py
+++ dagger_contrib/serializer/path/__init__.py
@@ -1,3 +1,4 @@
 """Collection of serializers for local directories or files."""
 
 from dagger_contrib.serializer.path.as_tar import AsTar  # noqa
+from dagger_contrib.serializer.path.as_zip import AsZip  # noqa
--- tests/serializer/path/test_as_zip.py
+++ tests/serializer/path/test_as_zip.py
@@ -5,19 +5,19 @@ import tempfile
 import pytest
 from dagger import DeserializationError, Serializer
 
-from dagger_contrib.serializer.path.as_tar import AsTar
+from dagger_contrib.serializer.path.as_zip import AsZip, _find_base_dir
 
 SUPPORTED_COMPRESSION_MODES = [
-    None,
-    "gzip",
-    "xz",
+    "stored",
+    "deflated",
+    "lzma",
     "bz2",
 ]
 
 
 def test__conforms_to_protocol():
     with tempfile.TemporaryDirectory() as tmp:
-        assert isinstance(AsTar(output_dir=tmp), Serializer)
+        assert isinstance(AsZip(output_dir=tmp), Serializer)
 
 
 def test_serialization_and_deserialization_are_symmetric_for_a_single_file():
@@ -32,15 +32,15 @@ def test_serialization_and_deserialization_are_symmetric_for_a_single_file():
 
             output_dir = os.path.join(tmp, "output_dir")
             os.mkdir(output_dir)
-            serializer = AsTar(output_dir=output_dir, compression=compression)
+            serializer = AsZip(output_dir=output_dir, compression=compression)
 
-            # The serializer produces a tar file
-            serialized_tar = os.path.join(tmp, f"serialized_tar.{serializer.extension}")
-            with open(serialized_tar, "wb") as writer:
+            # The serializer produces a zip file
+            serialized_zip = os.path.join(tmp, f"serialized.{serializer.extension}")
+            with open(serialized_zip, "wb") as writer:
                 serializer.serialize(original_file, writer)
 
             # And it can read it back
-            with open(serialized_tar, "rb") as reader:
+            with open(serialized_zip, "rb") as reader:
                 deserialized_file = serializer.deserialize(reader)
 
             # Retrieving a value equivalent to the original one (a filename pointing to the original content)
@@ -67,15 +67,15 @@ def test_serialization_and_deserialization_are_symmetric_for_a_directory():
 
             output_dir = os.path.join(tmp, "output_dir")
             os.mkdir(output_dir)
-            serializer = AsTar(output_dir=output_dir, compression=compression)
+            serializer = AsZip(output_dir=output_dir, compression=compression)
 
-            # The serializer produces a tar file
-            serialized_tar = os.path.join(tmp, f"serialized_tar.{serializer.extension}")
-            with open(serialized_tar, "wb") as writer:
+            # The serializer produces a zip file
+            serialized_zip = os.path.join(tmp, f"serialized.{serializer.extension}")
+            with open(serialized_zip, "wb") as writer:
                 serializer.serialize(original_dir, writer)
 
             # And it can read it back
-            with open(serialized_tar, "rb") as reader:
+            with open(serialized_zip, "rb") as reader:
                 deserialized_dir = serializer.deserialize(reader)
 
             # Retrieving a value equivalent to the original one (a directory
@@ -94,7 +94,7 @@ def test_serialization_and_deserialization_are_symmetric_for_a_directory():
                     assert f.read() == filename
 
 
-def test_deserialize_invalid_tar_file():
+def test_deserialize_invalid_zip_file():
     invalid_values = [
         b"",
         b"123",
@@ -103,7 +103,7 @@ def test_deserialize_invalid_tar_file():
     for value in invalid_values:
         for compression in SUPPORTED_COMPRESSION_MODES:
             with tempfile.TemporaryDirectory() as tmp:
-                serializer = AsTar(output_dir=tmp, compression=compression)
+                serializer = AsZip(output_dir=tmp, compression=compression)
 
                 with pytest.raises(DeserializationError):
                     serializer.deserialize(io.BytesIO(value))
@@ -111,16 +111,16 @@ def test_deserialize_invalid_tar_file():
 
 def test_extension_depends_on_compression():
     cases = [
-        (None, "tar"),
-        ("gzip", "tar.gz"),
-        ("xz", "tar.xz"),
-        ("bz2", "tar.bz2"),
+        ("deflated", "zip.zlib"),
+        ("stored", "zip"),
+        ("bz2", "zip.bz2"),
+        ("lzma", "zip.lzma"),
     ]
 
     for compression, expected_extension in cases:
         with tempfile.TemporaryDirectory() as tmp:
             assert (
-                AsTar(output_dir=tmp, compression=compression).extension
+                AsZip(output_dir=tmp, compression=compression).extension
                 == expected_extension
             )
 
@@ -128,4 +128,35 @@ def test_extension_depends_on_compression():
 def test_extension_fails_when_compression_is_not_supported():
     with pytest.raises(AssertionError):
         with tempfile.TemporaryDirectory() as tmp:
-            AsTar(output_dir=tmp, compression="unsupported")
+            AsZip(output_dir=tmp, compression="unsupported")
+
+
+def test_find_base_dir():
+    cases = [
+        {
+            "paths": ["a"],
+            "expected_result": "a",
+        },
+        {
+            "paths": ["a", "b"],
+            "expected_result": "",
+        },
+        {
+            "paths": [
+                os.path.join("subdir", "sub", "a"),
+                os.path.join("subdir", "sub", "b"),
+            ],
+            "expected_result": "subdir",
+        },
+        {
+            "paths": [
+                os.path.join("subdir", "sub", "a"),
+                os.path.join("subdir", "sub", "b"),
+                os.path.join("subdir", "x"),
+            ],
+            "expected_result": "subdir",
+        },
+    ]
+
+    for case in cases:
+        assert _find_base_dir(case["paths"]) == case["expected_result"]
